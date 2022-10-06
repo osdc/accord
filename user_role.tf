32ef19799a8f9dde83909a00e476d80ca1575d72
Arvind PJ <arvindpunk@gmail.com>
2022-10-06 11:08:25 +0530

    chore: add all sponsor members

--- user_role.tf
+++ user_role.tf
@@ -15,9 +15,66 @@
 #
 # -----------------------------------------------------------------------------
 
+
+resource "discord_member_roles" "aarkimos" {
+  server_id = data.discord_server.server.id
+  user_id   = data.discord_member.aarkimos.id
+  role {
+    role_id = data.discord_role.sponsor.id
+  }
+}
+
+resource "discord_member_roles" "akshgpt7" {
+  server_id = data.discord_server.server.id
+  user_id   = data.discord_member.akshgpt7.id
+  role {
+    role_id = data.discord_role.sponsor.id
+  }
+}
+
 resource "discord_member_roles" "arvindpunk" {
   server_id = data.discord_server.server.id
   user_id   = data.discord_member.arvindpunk.id
+  role {
+    role_id = data.discord_role.sponsor.id
+  }
+}
+
+resource "discord_member_roles" "daemon1024" {
+  server_id = data.discord_server.server.id
+  user_id   = data.discord_member.daemon1024.id
+  role {
+    role_id = data.discord_role.sponsor.id
+  }
+}
+
+resource "discord_member_roles" "humancalico" {
+  server_id = data.discord_server.server.id
+  user_id   = data.discord_member.humancalico.id
+  role {
+    role_id = data.discord_role.sponsor.id
+  }
+}
+
+resource "discord_member_roles" "ishikajn8126" {
+  server_id = data.discord_server.server.id
+  user_id   = data.discord_member.ishikajn8126.id
+  role {
+    role_id = data.discord_role.sponsor.id
+  }
+}
+
+resource "discord_member_roles" "rexagod" {
+  server_id = data.discord_server.server.id
+  user_id   = data.discord_member.rexagod.id
+  role {
+    role_id = data.discord_role.sponsor.id
+  }
+}
+
+resource "discord_member_roles" "Shreyas220" {
+  server_id = data.discord_server.server.id
+  user_id   = data.discord_member.Shreyas220.id
   role {
     role_id = data.discord_role.sponsor.id
   }
--- users.tf
+++ users.tf
@@ -8,7 +8,41 @@
 #
 # -----------------------------------------------------------------------------
 
+data "discord_member" "aarkimos" {
+  server_id = data.discord_server.server.id
+  user_id   = 509464333709869068
+}
+
+data "discord_member" "akshgpt7" {
+  server_id = data.discord_server.server.id
+  user_id   = 593562524150923301
+}
+
 data "discord_member" "arvindpunk" {
   server_id = data.discord_server.server.id
   user_id   = 219439471991586816
+}
+
+data "discord_member" "daemon1024" {
+  server_id = data.discord_server.server.id
+  user_id   = 531516025233997835
+}
+
+data "discord_member" "humancalico" {
+  server_id = data.discord_server.server.id
+  user_id   = 818171543758045204
+}
+
+data "discord_member" "ishikajn8126" {
+  server_id = data.discord_server.server.id
+  user_id   = 806936957254762517
+}
+
+data "discord_member" "rexagod" {
+  server_id = data.discord_server.server.id
+  user_id   = 476878010855981056
+}
+data "discord_member" "Shreyas220" {
+  server_id = data.discord_server.server.id
+  user_id   = 521686510907555866
 }
