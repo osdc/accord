20697f25d22a672b0f5684b28dc3c19805a6aff0
Berserkshit <114827680+Berserkshit@users.noreply.github.com>
2022-10-11 19:07:49 +0530

    Create Berserkshit.tf

---
+++ modules/users/Berserkshit.tf
@@ -0,0 +1,12 @@
+data "discord_member" "Monkey 7" {
+  server_id = data.discord_server.server.id
+  user_id   = 1022357593538179082
+}
+
+resource "discord_member_roles" "Monkey 7" {
+  server_id = data.discord_server.server.id
+  user_id   = data.discord_member.Monkey 7.id
+  role {
+    role_id = module.roles.hacktoberfest.id
+  }
+}
