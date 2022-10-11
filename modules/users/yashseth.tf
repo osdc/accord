08d8318070514be7992e183fac189187ac8c4ca6
Barun Acharya <barun1024@gmail.com>
2022-10-12 00:18:18 +0530

    fix incorrect user modules

--- modules/users/yashseth.tf
+++ modules/users/yashseth.tf
@@ -1,11 +1,11 @@
-data "discord_member" "yash seth" {
+data "discord_member" "yashseth391" {
   server_id = data.discord_server.server.id
   user_id   = 779247996973613056
 }
 
-resource "discord_member_roles" "yash seth" {
+resource "discord_member_roles" "yashseth391" {
   server_id = data.discord_server.server.id
-  user_id   = data.discord_member.yash seth.id
+  user_id   = data.discord_member.yashseth391.id
   role {
     role_id = module.roles.hacktoberfest.id
   }
