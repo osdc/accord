181c0e409b44001f1dff649bd446f3faaf1b867b
Berserkshit <114827680+Berserkshit@users.noreply.github.com>
2022-10-13 03:11:19 +0530

    Created Berserkshit.tf (#31)
    * Create Berserkshit.tf
    
    * Update Berserkshit.tf
    
    * Update Berserkshit.tf
    
    Co-authored-by: Karanjot Singh <thestrangequarks@protonmail.com>

---
+++ modules/users/Berserkshit.tf
@@ -0,0 +1,12 @@
+data "discord_member" "Monkey7" {
+  server_id = data.discord_server.server.id
+  user_id   = 1022357593538179082
+}
+
+resource "discord_member_roles" "Monkey7" {
+  server_id = data.discord_server.server.id
+  user_id   = data.discord_member.Monkey7.id
+  role {
+    role_id = module.roles.hacktoberfest.id
+  }
+}
