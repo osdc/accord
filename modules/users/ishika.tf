34160872c0c8936fe07d59fad102138cfdaff1c5
Karanjot Singh <thestrangequarks@protonmail.com>
2022-10-12 16:04:31 +0530

    Update ishika.tf

--- modules/users/ishika.tf
+++ modules/users/ishika.tf
@@ -4,7 +4,7 @@ data "discord_member" "Ishika" {
 }
 
 resource "discord_member_roles" "Ishika" {
-  server_id = data.discord_server.er.id
+  server_id = data.discord_server.server.id
   user_id   = data.discord_member.Ishika.id
   role {
     role_id = module.roles.hacktoberfest.id
