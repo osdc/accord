f9f731bc2e253bcf397029829529ca4914a34a93
Karanjot Singh <thestrangequarks@protonmail.com>
2022-10-13 03:09:30 +0530

    Update Berserkshit.tf

--- modules/users/Berserkshit.tf
+++ modules/users/Berserkshit.tf
@@ -1,9 +1,9 @@
-data "discord_member" "Monkey 7" {
+data "discord_member" "Monkey7" {
   server_id = data.discord_server.server.id
   user_id   = 1022357593538179082
 }
 
-resource "discord_member_roles" "Monkey 7" {
+resource "discord_member_roles" "Monkey7" {
   server_id = data.discord_server.server.id
   user_id   = data.discord_member.Monkey 7.id
   role {
