068516f40c3cd3378786d2039cc2d2b254ba8225
abhis1n <114600095+abhis1n@users.noreply.github.com>
2022-10-12 22:37:24 +0530

    Removed spaces from username

--- modules/users/FallenFaith.tf
+++ modules/users/FallenFaith.tf
@@ -1,11 +1,11 @@
-data "discord_member" "Fallen\ Faith" {
+data "discord_member" "FallenFaith" {
   server_id = data.discord_server.server.id
   user_id   = 479153009360764930
 }
 
-resource "discord_member_roles" "Fallen\ Faith" {
+resource "discord_member_roles" "FallenFaith" {
   server_id = data.discord_server.server.id
-  user_id   = data.discord_member.Fallen\ Faith.id
+  user_id   = data.discord_member.FallenFaith.id
   role {
     role_id = module.roles.hacktoberfest.id
   }
