cc2ba623c8f9e0a47736e504b694050642d7c972
Ishika Jain <72145053+ishikajn8126@users.noreply.github.com>
2022-10-12 18:32:33 +0530

    removed spaces in username

--- modules/users/prachinigam.tf
+++ modules/users/prachinigam.tf
@@ -1,11 +1,11 @@
-data "discord_member" "Prachi Nigam" {
+data "discord_member" "prachinigam20" {
   server_id = data.discord_server.server.id
   user_id   = 1025843733113086038
 }
 
-resource "discord_member_roles" "Prachi Nigam" {
+resource "discord_member_roles" "prachinigam20" {
   server_id = data.discord_server.server.id
-  user_id   = data.discord_member.Prachi Nigam.id
+  user_id   = data.discord_member.prachinigam20.id
   role {
     role_id = module.roles.hacktoberfest.id
   }
