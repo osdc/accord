5d082e9fa9c6af2b614d23c63eea83b0d4d0d5f0
Guillotine189 <114574134+Guillotine189@users.noreply.github.com>
2022-10-25 13:49:21 +0530

    Removed Guillotine189's role

--- modules/users/Guillotine189.tf
+++ modules/users/Guillotine189.tf
@@ -1,12 +1,1 @@
-data "discord_member" "Guillotine189" {
-  server_id = data.discord_server.server.id
-  user_id   = 858395536386293761
-}
 
-resource "discord_member_roles" "Guillotine189" {
-  server_id = data.discord_server.server.id
-  user_id   = data.discord_member.Guillotine189.id
-  role {
-    role_id = module.roles.hacktoberfest.id
-  }
-}
